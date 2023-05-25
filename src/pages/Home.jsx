122f1f69c03cac88fd398d866a1a3e4928a3bc23
Oniyide Abdurrazzaaq <arraazyy@gmail.com>
2023-05-25 19:23:51 +0100

    adding

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-
+import Announcement from '../component/Announcement'
+import Slider from '../component/Slider'
+import Navbar from '../component/Navbar'
 const Home = () => {
   return (
-    <div><Navbar/></div>
+    <div>
+      <Announcement/>
+      <Navbar/>
+      <Slider/>
+      </div>
   )
 }
 
